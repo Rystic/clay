f80810ffd5636cfbfc67de4e81b42de63037c9bc
Rystic <chris.bielak@gmail.com>
2013-12-09 03:38:41 -0300

    Added new background image
    New sky image for background.

--- src/city/effects/BackgroundEffect.java
+++ src/city/effects/BackgroundEffect.java
@@ -1,8 +1,13 @@
 package city.effects;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 import main.ClayConstants;
 
 import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
 
 import screens.AbstractScreen;
 
@@ -11,6 +16,20 @@ public class BackgroundEffect extends AbstractEffect
 	public BackgroundEffect(AbstractScreen homeScreen_)
 	{
 		super(homeScreen_);
+	}
+	
+	@Override
+	public void init()
+	{
+		try
+		{
+			_skyTexture = TextureLoader.getTexture(
+					"PNG",
+					new FileInputStream(new File("art/sky3.jpg")));
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
@@ -23,13 +42,29 @@ public class BackgroundEffect extends AbstractEffect
 //		GL11.glColor3f(0.45f, 0.45f, .42f);
 
 		// Draw the sky.
+//		GL11.glBegin(GL11.GL_POLYGON);
+//		GL11.glVertex2d(0, ClayConstants.DEFAULT_MAP_HEIGHT);
+//		GL11.glVertex2d(ClayConstants.DEFAULT_MAP_WIDTH, ClayConstants.DEFAULT_MAP_HEIGHT);
+//		GL11.glVertex2d(ClayConstants.DEFAULT_MAP_WIDTH, TILE_Y);
+//		GL11.glVertex2d(0, TILE_Y);
+//		GL11.glEnd();
+		
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, _skyTexture.getTextureID());
+		// continue;
+
 		GL11.glBegin(GL11.GL_POLYGON);
+		GL11.glTexCoord2f(0, 0);
 		GL11.glVertex2d(0, ClayConstants.DEFAULT_MAP_HEIGHT);
+		GL11.glTexCoord2f(.50f, 0);
 		GL11.glVertex2d(ClayConstants.DEFAULT_MAP_WIDTH, ClayConstants.DEFAULT_MAP_HEIGHT);
+		GL11.glTexCoord2f(.50f, .50f);
 		GL11.glVertex2d(ClayConstants.DEFAULT_MAP_WIDTH, TILE_Y);
+		GL11.glTexCoord2f(0, .50f);
 		GL11.glVertex2d(0, TILE_Y);
 		GL11.glEnd();
 	}
 
 	private static final int TILE_Y = ClayConstants.TILE_Y;
+	
+	private static Texture _skyTexture;
 }
