d52d3d80ea715064a49b4d2d132ce38460ff406a
Rystic <chris.bielak@gmail.com>
2014-05-13 18:20:01 -0400

    (Issue #32) GUI: Storage Menu
    Pressing 'space' switches to the storage menu, which keeps track of how
    many unclaimed items exist among your buildings.

--- src/city/ui/menus/areas/StorageInventoryArea.java
+++ src/city/ui/menus/areas/StorageInventoryArea.java
@@ -2,6 +2,8 @@ package city.ui.menus.areas;
 
 import java.util.Map;
 
+import org.newdawn.slick.Color;
+
 import models.CityModel;
 import screens.AbstractScreen;
 import city.generics.GenericItem;
@@ -16,24 +18,38 @@ public class StorageInventoryArea extends AbstractArea
 		super(homeScreen_);
 		_model = (CityModel) _homeScreen.getModel();
 		_yPos = yPos_;
+		_storageMenuLabel = new TextComponent(70, _yPos + 25,
+				"Storage Information");
+		_storageMenuLabel.setTextColor(Color.yellow);
+		_noItemsLabel = new TextComponent(72, _yPos - 15,
+				"No items in storage.");
+		_noItemsLabel.setTextColor(Color.red);
+
 	}
 
 	@Override
 	public void update()
 	{
 		_components.clear();
+		_components.add(_storageMenuLabel);
 		Map<String, Integer> itemInventory = _model.getItemInventory();
+		if (itemInventory.keySet().isEmpty())
+			_components.add(_noItemsLabel);
+
 		int yDiff = 0;
 		for (String key : itemInventory.keySet())
 		{
 			GenericItem item = ItemData.getItem(key);
-			_components.add(new ImageComponent(20, _yPos - yDiff - 10, 30, 30, item.getTexture()));
-			_components.add(new TextComponent(50, _yPos
-					- yDiff + 15, item.getItemName() + " - " + itemInventory.get(key)));
+			_components.add(new ImageComponent(20, _yPos - yDiff - 40, 30, 30,
+					item.getTexture()));
+			_components.add(new TextComponent(50, _yPos - yDiff - 15, item
+					.getItemName() + " - " + itemInventory.get(key)));
 			yDiff += 30;
 		}
 	}
-	
+
+	private TextComponent _storageMenuLabel;
+	private TextComponent _noItemsLabel;
 	private CityModel _model;
 	private int _yPos;
 }
